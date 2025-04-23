513ef84fce11dbe130167228ed36cd217877179b
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2025-04-23 10:27:16 +0500

    start repo

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-3xl text-red-500 text-center">
+    APP VUE
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/css/main.css'
+import './assets/css/base.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
