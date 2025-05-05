628a450c2cd0f55ac34f2cadeed5f7fed231fef8
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2025-05-05 11:46:10 +0500

    asdasd

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    Header
+  <div class="nav">
+      <div class="nav__top">
+        <div class="container">
+          <div class="logo"></div>
+          <div class="links">
+            <router-link to="/">Home</router-link>
+            <router-link to="/properties">Properties</router-link>
+            <router-link to="/contacts">Contacts</router-link>
+          </div>
+          <div class="login"></div>
+        </div>
+      </div>
+      <div class="nav__bottom"></div>
   </div>
 </template>
 
